7a497205a3c72e4dda5b5c33392d9d7e80f71687
Spazier1998 <137161302+Spazier1998@users.noreply.github.com>
2023-07-01 12:47:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-const dropZone = document.getElementById('drop-zone');
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-const fileInput = document.getElementById('file-input');
+const photoContainer = document.getElementById('photo-container');
+const existingPhoto = document.getElementById('existing-photo');
+const uploadedPhoto = document.getElementById('uploaded-photo');
 
 // 阻止浏览器默认的拖放行为
-dropZone.addEventListener('dragover', (event) => {
+photoContainer.addEventListener('dragover', (event) => {
   event.preventDefault();
 });
 
 // 处理拖放事件
-dropZone.addEventListener('drop', (event) => {
+photoContainer.addEventListener('drop', (event) => {
   event.preventDefault();
 
   const file = event.dataTransfer.files[0];
@@ -27,21 +26,43 @@ function handleImage(file) {
   const reader = new FileReader();
 
   reader.onload = function (event) {
-    const image = new Image();
-    image.onload = function () {
-      canvas.width = image.width;
-      canvas.height = image.height;
-      context.drawImage(image, 0, 0);
+    uploadedPhoto.src = event.target.result;
+    uploadedPhoto.onload = function () {
+      // 允许改变大小和位置
+      uploadedPhoto.setAttribute('draggable', 'true');
+      uploadedPhoto.style.cursor = 'move';
 
-      // 在这里可以添加照片合并的逻辑
-      // 可以使用 context.drawImage() 将图片绘制到 canvas 上
+      // 通过事件监听器实现拖动和调整大小的功能
+      uploadedPhoto.addEventListener('dragstart', dragStart);
+      uploadedPhoto.addEventListener('dragend', dragEnd);
+      uploadedPhoto.addEventListener('mousedown', mouseDown);
+      uploadedPhoto.addEventListener('mouseup', mouseUp);
 
-      // 示例：绘制一个红色矩形
-      context.fillStyle = 'red';
-      context.fillRect(0, 0, 100, 100);
+      // 将合并后的照片保存为文件
+      saveMergedImage();
     };
-    image.src = event.target.result;
   };
 
   reader.readAsDataURL(file);
 }
+
+// 保存合并后的照片
+function saveMergedImage() {
+  const canvas = document.createElement('canvas');
+  canvas.width = photoContainer.offsetWidth;
+  canvas.height = photoContainer.offsetHeight;
+  const context = canvas.getContext('2d');
+
+  context.drawImage(existingPhoto, 0, 0);
+  context.drawImage(uploadedPhoto, uploadedPhoto.offsetLeft, uploadedPhoto.offsetTop, uploadedPhoto.offsetWidth, uploadedPhoto.offsetHeight);
+
+  const mergedImage = canvas.toDataURL('image/png');
+  const downloadLink = document.createElement('a');
+  downloadLink.href = mergedImage;
+  downloadLink.download = 'merged_image.png';
+  downloadLink.click();
+}
+
+// 拖动开始时的处理函数
+function dragStart(event) {
+  this.style.opacity
